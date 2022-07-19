f59c8a58876c8204b021f85d2239b5ff01c5cc38
Jared Hardinger <jaredhardinger@gmail.com>
2022-07-19 17:05:55 -0400

    Add test for total cost on recipe show page

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe 'Recipes Show Page' do
     expect(page).to have_content(cheese.name)
     expect(page).to have_content(sauce.name)
   end
+
+  it 'shows the total cost of all the ingredients in the recipe' do
+    recipe_1 = Recipe.create!(name: 'Pizza', complexity: '4', genre: 'Italian')
+    dough = Ingredient.create!(name: 'Dough', cost: '1')
+    cheese = Ingredient.create!(name: 'Cheese', cost: '3')
+    sauce = Ingredient.create!(name: 'Sauce', cost: '2')
+    RecipeIngredient.create!(recipe: recipe_1, ingredient: dough)
+    RecipeIngredient.create!(recipe: recipe_1, ingredient: cheese)
+    RecipeIngredient.create!(recipe: recipe_1, ingredient: sauce)
+
+    visit "/recipes/#{recipe_1.id}"
+
+    expect(page).to have_content("Total Cost: 6")
+  end
 end
